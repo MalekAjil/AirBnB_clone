4e759d3f513d9d440f59b9def2bde752e04bf830
010binary <augustine32fg@gmail.com>
2024-02-08 05:40:30 -0800

    Adding Task 4 and also adding the __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,41 @@
 """
-Base Model
+A BaseModel class that defines all common attributes/methods for other classes
 """
 
-class base:
-    id = 0
+from datetime import datetime
+import uuid
+
+class BaseModel:
+   """
+   Class initialization
+   """ 
+    def __init__(self, *args, **kwargs):
+        """
+        Initialing 
+        Created_at, id, Updated_at and the time_format instance
+
+        Created_at, Updated_at are set to the isoformat using the .isoformat
+        method
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow().isoformat()
+        self.updated_at = datetime.utcnow().isoformat()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    self.__dict__[key] = datetime.strptime(value,
+                    DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        """
+        Prints the string representation of 
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        cls_name = self.__class__.__name__
+        return f"[cls_name] (self.id) self.__dict__"
