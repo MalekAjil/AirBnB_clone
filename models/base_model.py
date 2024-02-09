4313c00443b7927c930324da6a425a1d43156c67
Alpha_01 <99992586+010binary@users.noreply.github.com>
2024-02-09 10:11:06 +0100

    Fixing the Indentation Issue

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,9 @@ from datetime import datetime
 import uuid
 
 class BaseModel:
-   """
-   Class initialization
-   """ 
+    """
+    Class initialization
+    """ 
     def __init__(self, *args, **kwargs):
         """
         Initialing 
@@ -17,7 +17,7 @@ class BaseModel:
         Created_at, Updated_at are set to the isoformat using the .isoformat
         method
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow().isoformat()
         self.updated_at = datetime.utcnow().isoformat()
@@ -25,9 +25,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key in ("created_at", "updated_at"):
-                    self.__dict__[key] = datetime.strptime(value,
-                    DATE_TIME_FORMAT)
-                elif key[0] == "id":
+                    self.__dict__[key] = datetime.strptime(value, DATE_TIME_FORMAT)
+                elif key == "id":
                     self.__dict__[key] = str(value)
                 else:
                     self.__dict__[key] = value
@@ -38,22 +37,20 @@ class BaseModel:
         [<class name>] (<self.id>) <self.__dict__>
         """
         cls_name = self.__class__.__name__
-        return f"[cls_name] (self.id) self.__dict__"
+        return f"[{cls_name}] ({self.id}) {self.__dict__}"
 
     def to_dict(self):
         """
-         returns a dictionary containing all keys/values
-         of __dict__ of the instance.
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance.
         """
-        dictionary_object={}
-        dictionary _object['__class__'] = self.__class__.__name__
+        dictionary_object = {}
+        dictionary_object['__class__'] = self.__class__.__name__
 
         # Convert created_at and updated_at to ISO format strings
-        if hasattr(self, 'created_at') and isinstance(self.created_at,
-        datetime.datetime):
+        if hasattr(self, 'created_at') and isinstance(self.created_at, datetime):
             dictionary_object['created_at'] = self.created_at.isoformat()
-        if hasattr(self, 'updated_at') and isinstance(self.updated_at,
-        datetime.datetime):
+        if hasattr(self, 'updated_at') and isinstance(self.updated_at, datetime):
             dictionary_object['updated_at'] = self.updated_at.isoformat()
 
         dictionary_object.update(self.__dict__)
