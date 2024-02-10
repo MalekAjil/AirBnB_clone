ca1030f31c6673f616a7e648da3eabfaa99bbaaa
MalekAjil <aboanasajil@gmail.com>
2024-02-10 09:44:33 -0800

    style

--- models/base_model.py
+++ models/base_model.py
@@ -5,17 +5,18 @@ A BaseModel class that defines all common attributes/methods for other classes
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """
     Class initialization
-    """ 
+    """
     def __init__(self, *args, **kwargs):
         """
-        Initialing 
+        Initialing
         Created_at, id, Updated_at and the time_format instance
 
-        Created_at, Updated_at are set to the isoformat using the .isoformat
-        method
+        Created_at, Updated_at are set to the isoformat using the
+        .isoformat method
         """
         DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
@@ -25,7 +26,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key in ("created_at", "updated_at"):
-                    self.__dict__[key] = datetime.strptime(value, DATE_TIME_FORMAT)
+                    self.__dict__[key] = datetime.strptime(value,
+                                                           DATE_TIME_FORMAT)
                 elif key == "id":
                     self.__dict__[key] = str(value)
                 else:
@@ -34,7 +36,7 @@ class BaseModel:
     def __str__(self):
         """
         private instance method
-        Prints the string representation of 
+        Prints the string representation of
         [<class name>] (<self.id>) <self.__dict__>
         """
         cls_name = self.__class__.__name__
@@ -49,9 +51,11 @@ class BaseModel:
         dictionary_object['__class__'] = self.__class__.__name__
 
         # Convert created_at and updated_at to ISO format strings
-        if hasattr(self, 'created_at') and isinstance(self.created_at, datetime):
+        if hasattr(self, 'created_at') and isinstance(self.created_at,
+                                                      datetime):
             dictionary_object['created_at'] = self.created_at.isoformat()
-        if hasattr(self, 'updated_at') and isinstance(self.updated_at, datetime):
+        if hasattr(self, 'updated_at') and isinstance(self.updated_at,
+                                                      datetime):
             dictionary_object['updated_at'] = self.updated_at.isoformat()
 
         dictionary_object.update(self.__dict__)
@@ -60,6 +64,7 @@ class BaseModel:
 
     def save(self):
         """
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute updated_at with
+        the current datetime
         """
         self.updated_at = datetime.utcnow().isoformat()
