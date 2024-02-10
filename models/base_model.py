1492ddaec6f813a1f90117b9da4c61cea5fc1600
010binary <augustine32fg@gmail.com>
2024-02-10 10:34:58 -0800

    fixing the Indentation error

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """
-        Initialing...
+        Initialing
         Created_at, id, Updated_at and the time_format instance.
         Created_at, Updated_at are set to the isoformat using the
         .isoformat method.
@@ -25,8 +25,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key in ("created_at", "updated_at"):
-                    self.__dict__[key] = datetime.strptime(value,
-                                                           DATE_TIME_FORMAT)
+                    self.__dict__[key] = datetime.strptime(value, DATE_TIME_FORMAT)
                 elif key == "id":
                     self.__dict__[key] = str(value)
                 else:
@@ -50,11 +49,9 @@ class BaseModel:
         dictionary_object['__class__'] = self.__class__.__name__
 
         # Convert created_at and updated_at to ISO format strings
-        if hasattr(self, 'created_at') and isinstance(self.created_at,
-                                                      datetime):
+        if hasattr(self, 'created_at') and isinstance(self.created_at, datetime):
             dictionary_object['created_at'] = self.created_at.isoformat()
-        if hasattr(self, 'updated_at') and isinstance(self.updated_at,
-                                                      datetime):
+        if hasattr(self, 'updated_at') and isinstance(self.updated_at, datetime):
             dictionary_object['updated_at'] = self.updated_at.isoformat()
 
         dictionary_object.update(self.__dict__)
