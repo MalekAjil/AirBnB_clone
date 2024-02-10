4526664d225e21745c312c9877015dac7f0feb52
MalekAjil <aboanasajil@gmail.com>
2024-02-10 10:11:52 -0800

    task6

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Command Interpreter Model"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class that contains the entry point of the command interpreter"""
+    prompt = "(hbnb) "
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def do_quit(self, args):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, args):
+        """EOF command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
