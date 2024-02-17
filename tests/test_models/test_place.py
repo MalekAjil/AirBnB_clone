fd316a7fb95916a32d51b7a581faeb04e4c58fc9
010binary <augustine32fg@gmail.com>
2024-02-17 04:30:06 +0100

    fixing my issues with basemodel

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,69 @@
 #!/usr/bin/python3
-""" """
+""" Testing the Places avaliable"""
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """ """
+    """ Class Init"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialization"""
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
-    def test_city_id(self):
-        """ """
+    def test_cityid(self):
+        """ Testing the City ID"""
         new = self.value()
         self.assertEqual(type(new.city_id), str)
 
-    def test_user_id(self):
-        """ """
+    def test_userid(self):
+        """ Testing the User ID"""
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
-        """ """
+        """ Testing the city name"""
         new = self.value()
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
-        """ """
+        """ Testing the description"""
         new = self.value()
         self.assertEqual(type(new.description), str)
 
-    def test_number_rooms(self):
-        """ """
+    def test_numberRooms(self):
+        """ Testing the number of rooms """
         new = self.value()
         self.assertEqual(type(new.number_rooms), int)
 
-    def test_number_bathrooms(self):
-        """ """
+    def test_numberBathrooms(self):
+        """ Testing the number bath rooms avaliable"""
         new = self.value()
         self.assertEqual(type(new.number_bathrooms), int)
 
-    def test_max_guest(self):
-        """ """
+    def test_maxguest(self):
+        """ Testing the maxiunm number of Guest """
         new = self.value()
         self.assertEqual(type(new.max_guest), int)
 
-    def test_price_by_night(self):
-        """ """
+    def test_price(self):
+        """ Testing the price"""
         new = self.value()
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
-        """ """
+        """ Testing the location  """
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
-        """ """
+        """Testing the location  """
         new = self.value()
         self.assertEqual(type(new.latitude), float)
 
-    def test_amenity_ids(self):
-        """ """
+    def test_amenityIds(self):
+        """ Testing the amenity_ids"""
         new = self.value()
         self.assertEqual(type(new.amenity_ids), list)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" testing the state Functionality """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """ TEst Class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Init for the steps"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
-    def test_name3(self):
-        """ """
+    def test_names(self):
+        """Test state names"""
         new = self.value()
         self.assertEqual(type(new.name), str)
